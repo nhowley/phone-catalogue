7724909b400229be8cb97ae8faa0c85db89f6e33
nhowley <nataliehowley@outlook.com>
2021-07-25 23:40:46 +0200

    Update routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,18 +1,12 @@
-// load up our shiny new route for phones
 const phoneRoutes = require('./phones');
 
 const appRouter = (app, fs) => {
-  // we've added in a default route here that handles empty routes
-  // at the base API url
   app.get('/', (req, res) => {
     res.send('Welcome to your rest API');
   });
-
-  // run our phone route module here to complete the wire up
   phoneRoutes(app, fs);
 };
 
-// this line is unchanged
 module.exports = appRouter;
 
 
